10bf737cefe9a6dfd28ddd9c24ac94e778caa14e
olga_komar <komar_ol@mail.ru>
2024-03-21 14:43:17 +0300

    refactoring classes

--- src/main/java/org/komar/client/ClientGUI.java
+++ src/main/java/org/komar/client/ClientGUI.java
@@ -3,45 +3,106 @@ package org.komar.client;
 import org.komar.server.ServerWindow;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class ClientGUI extends JFrame implements ActionListener {
+public class ClientGUI extends JFrame implements ClientView, ActionListener {
     private static final int WIDTH = 300;
     private static final int HEIGHT = 507;
-    private JPanel jMsgPanel;
 
+    private JPanel jMsgPanel;
     private JTextField jTextField;
     private JButton sendBtn;
     private JButton loginBtn;
     private JTextField textLogin;
     private JPanel jPanel;
-    private CardLayout cardLayout;
-    private JTextArea textArea;
+    private JPanel jPanelLogin;
+    private JPanel jLoginPanel;
+    private JPanel jTextPanel;
+
+    private JTextArea jTextArea;
+    private JScrollPane jScrollPane;
+    private JTextField localPort;
+    private JTextField port;
+    private JPasswordField passwordField;
     private ServerWindow serverWindow;
     private boolean isStart = false;
 
     public ClientGUI(ServerWindow serverWindow) {
         this.serverWindow = serverWindow;
 
+        setting();
+        createPanel();
+
+        this.setVisible(true);
+    }
+
+    private void setting() {
         this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
-        this.cardLayout = new CardLayout();
 
         this.setTitle("Client chat");
         this.setResizable(false);
+    }
 
-        this.jPanel = new JPanel(cardLayout);
-        InitialClientPanel icp = new InitialClientPanel();
-        jPanel.add(icp);
-        this.loginBtn = icp.getLoginBtn();
-        this.textLogin = icp.getTextLogin();
-        this.add(jPanel, BorderLayout.CENTER);
+    private void createPanel() {
+        add(createHeaderPanel(), BorderLayout.NORTH);
+        add(createLog(), BorderLayout.CENTER);
+        add(createFooter(), BorderLayout.SOUTH);
+    }
 
+
+    private JPanel createHeaderPanel() {
+        this.jLoginPanel = new JPanel(new GridLayout(2, 3, 2, 2));
+        this.localPort = new JTextField("127.0.0.1");
+        this.port = new JTextField("8189");
+        this.textLogin = new JTextField();
+        this.textLogin.setToolTipText("Введите ваше имя...");
+        this.passwordField = new JPasswordField();
+        this.loginBtn = new JButton("Login");
+        this.jLoginPanel.add(localPort);
+        this.jLoginPanel.add(port);
+        this.jLoginPanel.add(new JLabel());
+        this.jLoginPanel.add(textLogin);
+        this.jLoginPanel.add(passwordField);
+        this.jLoginPanel.add(loginBtn);
+        this.loginBtn.addActionListener(this);
+        return jLoginPanel;
+    }
+
+    private JPanel createLog() {
+        this.jTextPanel = new JPanel(new GridLayout(1, 1));
+        this.jTextPanel.setBorder(new EmptyBorder(5, 5, 3, 3));
+
+        this.jTextArea = new JTextArea();
+        this.jTextArea.setLineWrap(true);
+
+        this.jScrollPane = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
+        jScrollPane.setVerticalScrollBar(jScrollPane.getVerticalScrollBar());
+        jScrollPane.setViewportView(jTextArea);
+
+        this.jTextPanel.add(jScrollPane);
+        return jTextPanel;
+    }
+
+    private JPanel createFooter() {
         this.jMsgPanel = new JPanel();
 
         this.jTextField = new JTextField();
+        jTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (e.getKeyChar() == '\n') {
+                    message();
+                }
+            }
+        });
         this.sendBtn = new JButton("Send");
         sendBtn.setEnabled(false);
 
@@ -50,15 +111,58 @@ public class ClientGUI extends JFrame implements ActionListener {
         jMsgPanel.add(jTextField);
         jMsgPanel.add(Box.createHorizontalStrut(5));
         jMsgPanel.add(sendBtn);
-        this.add(jMsgPanel, BorderLayout.SOUTH);
-
         this.sendBtn.addActionListener(this);
-        this.loginBtn.addActionListener(this);
-
-        this.setVisible(true);
+        return jMsgPanel;
     }
 
+    /**
+     * Метод вывода текста на экран GUI. Вызывается из контроллера
+     * @param msg текст, который требуется отобразить на экране
+     */
+    @Override
+    public void showMessage(String msg) {
+        log.append(msg + "\n");
+    }
 
+    /**
+     * Метод, описывающий отключение клиента от сервера со стороны сервера
+     */
+    @Override
+    public void disconnectedFromServer(){
+        hideHeaderPanel(true);
+    }
+
+    /**
+     * Метод, описывающий отключение клиента от сервера со стороны клиента
+     */
+    public void disconnectFromServer(){
+        clientController.disconnectFromServer();
+    }
+
+    /**
+     * Метод изменения видимости верхней панели экрана, на которой виджеты для авторизации (например кнопка логин)
+     * @param visible true, если надо сделать панель видимой
+     */
+    public void hideHeaderPanel(boolean visible){
+        headerPanel.setVisible(visible);
+    }
+
+    /**
+     * Метод, срабатывающий при нажатии кнопки авторизации
+     */
+    public void login(){
+        if (clientController.connectToServer(tfLogin.getText())){
+            headerPanel.setVisible(false);
+        }
+    }
+
+    /**
+     * Метод для отправки сообщения. Используется при нажатии на кнопку send
+     */
+    private void message(){
+        clientController.message(tfMessage.getText());
+        tfMessage.setText("");
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -66,10 +170,6 @@ public class ClientGUI extends JFrame implements ActionListener {
             if (serverWindow.isStart()) {
                 if (!textLogin.getText().trim().isEmpty()) {
                     serverWindow.sendMsgOnServer("Клиент " + textLogin.getText() + " подключился");
-                    StartClientPanel scp = new StartClientPanel(serverWindow);
-                    textArea = scp.getJTextArea();
-                    this.jPanel.add(scp);
-                    cardLayout.next(jPanel);
                     sendBtn.setEnabled(true);
                 } else {
                     JOptionPane.showMessageDialog(null, "Введите логин", "Error", JOptionPane.ERROR_MESSAGE);
@@ -82,12 +182,22 @@ public class ClientGUI extends JFrame implements ActionListener {
             if (serverWindow.isStart()) {
                 if (!jTextField.getText().trim().isEmpty()) {
                     serverWindow.sendMsgOnServer(textLogin.getText()+": "+jTextField.getText());
-                    textArea.setText(serverWindow.initMsg());
+                    jTextArea.setText(serverWindow.initMsg());
                 }
             } else {
                 JOptionPane.showMessageDialog(null, "Сервер не запущен", "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
+
+    }
+
+    @Override
+    public void showMessage(String message) {
+
+    }
+
+    @Override
+    public void disconnectedFromServer() {
 
     }
 }
---
+++ src/main/java/org/komar/client/ClientView.java
@@ -0,0 +1,6 @@
+package org.komar.client;
+
+public interface ClientView {
+    void showMessage(String message);
+    void disconnectedFromServer();
+}
